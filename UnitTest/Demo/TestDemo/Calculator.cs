c8891c9b7aee20763abe1e3a9fb792779fa9dcbc
ust oldfield <uoldfield@gmail.com>
2018-12-13 16:01:18 +0000

    adding unit test demo

---
+++ UnitTest/Demo/TestDemo/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDemo
+{
+    public class Calculator
+    {
+        static public int AddNumber(int numberOne, int numberTwo)
+        {
+            int result = numberOne + numberTwo;
+            return result;       
+        }
+    }
+}
---
+++ UnitTest/Demo/TestDemoTests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDemo.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddNumberTest_10Plus20Equal30()
+        {
+            int r = Calculator.AddNumber(10, 20);
+            Assert.AreEqual(30, r);
+        }
+    }
+}
+
